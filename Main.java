4201d537ba8379b3672885d39bc9241ef98dd6ab
Andrei <andreirech22@outlook.com>
2024-06-18 14:16:29 -0300

    adicionando funcoes de tempo e leitura de arquivo

--- Main.java
+++ Main.java
@@ -1,48 +1,50 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         maiorCaminho();
     }
 
-    private static Box [] criaCaixas() {
-        Box[] caixas = {
-            new Box(991, 443, 126),
-            new Box(733, 281, 710),
-            new Box(910, 720, 218),
-            new Box(743, 512, 162),
-            new Box(988, 955, 720),
-            new Box(680, 603, 649),
-            new Box(336, 326, 615),
-            new Box(566, 764, 487),
-            new Box(680, 579, 148),
-            new Box(629, 222, 697)
-        };
-        return caixas;
+    private static Map<List<Integer>, String> mapFiles() {
+        Map<List<Integer>, String> casos = new HashMap<>();
+        casos.put(Arrays.asList(10,4), "Exemplo");
+        //casos.put(Arrays.asList(10,4), "00010");
+        //casos.put(Arrays.asList(20,5), "00020");
+        //casos.put(Arrays.asList(50,8), "00050");
+        //casos.put(Arrays.asList(100,13), "00100");
+        //casos.put(Arrays.asList(200,18), "00200");
+        //casos.put(Arrays.asList(300,19), "00300");
+        //casos.put(Arrays.asList(500,23), "00500");
+        //casos.put(Arrays.asList(1000,28), "01000");
+        //casos.put(Arrays.asList(2000,44), "02000");
+        //casos.put(Arrays.asList(5000,61), "05000");
+        //casos.put(Arrays.asList(10000,76), "10000");
+
+        return casos;
     }
-
-    private static Digraph criaDigrafo() {
-        Digraph digraph = new Digraph();
-        Box[] caixas = criaCaixas();
-
-        for(int i=0; i<caixas.length; i++) {
-            for(int j=0; j<caixas.length; j++) {
-                if(i != j && caixas[i].fitInto(caixas[j])) {
-                    digraph.addEdge(String.valueOf(i), String.valueOf(j));
-                }
-            }
-        }
-
-        return digraph;
-    }
-
+        
     private static void maiorCaminho() {
-        Digraph digraph = criaDigrafo();
-        DFS dfs = new DFS(digraph, "0");
+        Map<List<Integer>, String> casos = mapFiles();
 
-        // Precisamos pensar em um jeito de fazer isso :(
-        // Provavelmente vai ser algo parecido com as Árvores de Caminho Mínimo
-        // Talvez usar um Dijkstra mas ao invez de peso das arestas, o distTo[] seria quantos nodos até o pai
-        // Ai só inverteriamos o resultado, pegando o pior não o melhor
+        casos.forEach((key, value) -> {
+            String filename = "casosT30\\caso" + value + ".txt";
 
-        // System.out.println(digraph.toDot());
+            long start = System.nanoTime();
+            
+            Digraph digraph = new Digraph(filename, key.get(0));
+            DFS dfs = new DFS(digraph);
+
+            long finish = System.nanoTime();
+
+            double finalTime = (finish - start) / (double) 1000000000;
+
+            System.out.println("\n----- [ CASO " +value +" ] -----");
+            System.out.println("-> Valor esperado [ " +key.get(1) +" ]");
+            System.out.println("-> Valor encontrado [ " +dfs.longestLenght() +" ]");
+            System.out.printf("-> Tempo [ %.3f segundos ]\n", finalTime);
+        });
     }
 }
